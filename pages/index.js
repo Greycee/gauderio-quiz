d3e8344fb067356de33d2b682d5f535ba43eae82
Greycee <greyceriquinho@gmail.com>
2021-01-28 19:32:35 -0300

    Commit changes from day 03

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,8 @@ import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 import Footer from '../src/components/Footer';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -44,7 +34,7 @@ export default function Home() {
             }}
             >
               <Input placeholder="Qual é teu nome, tchê?" onChange={(e) => setName(e.target.value)} value={name} />
-              <Button type="submit" disabled={name.length === 0}>Jogar!</Button>
+              <Button type="submit" disabled={name.length === 0}>Testar!</Button>
             </form>
           </Widget.Content>
         </Widget>
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,7 +1,90 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState, useEffect } from 'react';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Button from '../src/components/Button';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+
+const QuestionWidget = ({ question, questionIndex, onSubmit }) => {
+  const questionId = `question_${questionIndex}`;
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>{`Pergunta ${questionIndex + 1} de ${db.questions.length}`}</h3>
+      </Widget.Header>
+      <img style={{ width: '100%', height: '150px', objectFit: 'cover' }} src={question.image} />
+      <Widget.Content>
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit();
+        }}
+        >
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative_${alternativeIndex}`;
+            return (
+              <Widget.Topic>
+                <input name={questionId} type="radio" id={alternativeId} style={{ display: 'none' }} />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+          <Button type="submit">Confirmar</Button>
+        </form>
+      </Widget.Content>
+    </Widget >
+  );
+};
+
+function LoadingWidget() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+    </Widget>
+  );
+}
+
+const screenStates = { QUIZ: 'QUIZ', LOADING: 'LOADING', RESULT: 'RESULT' };
 
 export default function QuizPage() {
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const questionIndex = currentQuestion;
+  const question = db.questions[questionIndex];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1000);
+  }, []);
+
+  const handleSubmit = () => {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < db.questions.length) {
+      setCurrentQuestion(questionIndex + 1);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  };
+
   return (
-    <div />
+    <QuizBackground>
+      <QuizContainer>
+        {screenState === screenStates.QUIZ
+          && (
+            <QuestionWidget
+              question={question}
+              questionIndex={questionIndex}
+              onSubmit={handleSubmit}
+            />
+          )}
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+      </QuizContainer>
+    </QuizBackground>
   );
 }
